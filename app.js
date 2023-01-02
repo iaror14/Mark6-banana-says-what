6150294c4f83fff4546cf0274a960cd9aea15020
iaror14 <isha.arora561@gmail.com>
2023-01-03 01:16:21 +0530

    JS

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 alert("This script works!");
+var username = prompt("What is your name?");
+alert("I hope it works! " + username);
+//input
+var click = prompt("What is username?");
+//processing
+var welcomeMessage = "It works! " + click;
+//output
+alert(welcomeMessage);
