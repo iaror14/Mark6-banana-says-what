36fe7adaf41eec2ff67bc599bc54948f93030eb3
iaror14 <isha.arora561@gmail.com>
2023-01-14 03:03:22 +0530

    updated now

--- app.js
+++ app.js
@@ -13,18 +13,28 @@ console.log(btnTranslate);
 var txtInput = document.querySelector("#txt-input");
 console.log(txtInput);
 
+var outputDiv = document.querySelector("#output-div");
+console.log(outputDiv);
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
 //temp1.addEventListener('click', function eventHandler() { console.log('clicked')})  - tried in console
 function clickHandler() {
   //create a function to ensure it can be called later to an event
+  var inputText = txtInput.value; //wasn't working becuase this was outside of the function, should have been inside only!
   //console.log("Clicked!"); //define what the function will do when it's called in the second fucntion!
-  //console.log("input", txtInput.value);
-  outputDiv.innerText = "blahblahblah  " + txtInput.value;
+  fetch(getTranslation(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    });
 }
 btnTranslate.addEventListener("click", clickHandler);
-
-var outputDiv = document.querySelector("#output-div");
-console.log(outputDiv);
-outputDiv.innerText;
 
 //Exercise responses!
 /*document.querySelector("textarea");
